98e9d1d77daa2a299fc78601a1a0998073e3cb8f
vanniafigueroa <juliett.fa@gmail.com>
2019-10-10 12:01:46 -0500

    primer avance

---
+++ src/index.js
@@ -0,0 +1,23 @@
+alert("Alguna vez pensaste hablar y lograr entender como la pelicula Matrix?")
+
+let ascii
+let operacion
+let total1
+let total2
+let total3
+let totales1
+let totales2
+let totales3
+let minuscula
+let sumatoria1=""
+let sumatoria2= ""
+let sumatoria3=""
+let suma1=""
+let suma2=""
+let suma3=""
+let result= ""
+
+
+document.getElementById("cifrar").addEventListener("click", cifrar)
+document.getElementById("descifrar").addEventListener("click", decode)
+
