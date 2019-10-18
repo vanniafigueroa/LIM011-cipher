15f4df5f4d03077cf3785a4da75a72fb1c1a8a72
vanniafigueroa <juliett.fa@gmail.com>
2019-10-18 08:23:20 -0500

    avance 4.0

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,31 @@
 window.cipher= {
     encode: (offset, string) => {
-        let ascii
-        let operacion
-        let total1
-        let sumatoria1=""
+        let positionAsciiOfLetter
+        let positionLetterCipher
+        let letterCipher
+        let resultFinal=""
 
         for (let i= 0; i < string.length; i++) {
-            ascii= string.charCodeAt(i)
-            if ( ascii >= 65 && ascii <= 90) {
-                operacion=((ascii - 65) +parseInt(offset))%26+65
-                total1= String.fromCharCode(operacion)
-                sumatoria1 += total1; 
+            positionAsciiOfLetter= string.charCodeAt(i)
+            if ( positionAsciiOfLetter >= 65 && positionAsciiOfLetter <= 90) {
+                positionLetterCipher=((positionAsciiOfLetter - 65) +parseInt(offset))%26+65
+                letterCipher= String.fromCharCode(positionLetterCipher)
+                resultFinal += letterCipher; 
             }    
         }
-       return sumatoria1
+       return resultFinal
     },
     decode: (offset, string) => {
-        let ascii
-        let operacion
-        let totales1
-        let suma1=""      
+        let positionAsciiOfLetter
+        let positionLetterCipher
+        let letterCipher
+        let resultFinal=""      
         for (let i= 0; i < string.length; i++) {
-            ascii= string.charCodeAt(i)
-            if ( ascii >= 65 && ascii <= 90) {
-                operacion=((ascii - 90) - parseInt(offset))%26+90
-                totales1= String.fromCharCode(operacion)
-                suma1 += totales1;
+            positionAsciiOfLetter= string.charCodeAt(i)
+            if ( positionAsciiOfLetter >= 65 && positionAsciiOfLetter <= 90) {
+                positionLetterCipher=((positionAsciiOfLetter - 90) - parseInt(offset))%26+90
+                letterCipher= String.fromCharCode(positionLetterCipher)
+                resultFinal += letterCipher;
             }  
         }
        return suma1
